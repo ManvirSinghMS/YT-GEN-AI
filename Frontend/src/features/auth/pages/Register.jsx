ec26cec4942ff773ee98483ae4abe47218d816ae
Manvir Singh <Manvirs1611@gmail.com>
2026-04-20 17:48:31 +0530

    Login and Register UI and Routing

---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {useNavigate} from 'react-router';
+const Register = () => {
+
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+    e.preventDefault()
+   }
+
+
+
+    return(
+<main>
+      <div className="form-container">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="Username">UserName</label>
+            <input
+              type="Username"
+              id="Username"
+              name="Username"
+              placeholder="Enter username"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter email address"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter Password"
+            />
+          </div>
+
+          <button className="button primary-button">Register</button>
+        </form>
+      </div>
+    </main>
+    )
+}
+
+export default Register
